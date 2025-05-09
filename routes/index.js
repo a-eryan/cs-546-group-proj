4d3b59f0db10bd9be93e2ba10f92b78db8294a21
rdhadda123 <rdhadda@stevens.edu>
2025-05-09 17:06:14 -0400

    Restyle study spots page and re-route home page after login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 import authRoutes from "./auth.js"
+import studySpotsRoutes from "./studySpots.js"
+import userRoutes from "./users.js"
 
 const constructorMethod = (app) => {
     app.use('/', authRoutes);
+    app.use('/', studySpotsRoutes);
+    app.use('/', userRoutes);
     app.use('*', (req, res) => {
         return res.status(404).json({error: 'Not found'});
     });
--- routes/studySpots.js
+++ routes/studySpots.js
@@ -6,8 +6,14 @@ router
   .route('/studyspots')
   .get(async(req,res) => {
     try {
-      const allSpots = await getAllStudySpots()
-      return res.render('studySpots/list', {spots: allSpots});
+      const allSpots = await getAllStudySpots();
+      const user = req.session.user || null;
+      const isSignedIn = !!user;
+      return res.render('studySpots/list', {
+        spots: allSpots,
+        isSignedIn: isSignedIn,
+        user: user
+      });
     } catch (e) {
       return res.status(400).render('studySpots/list', { error: e });
     }
