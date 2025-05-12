a0ba2a5305d5d486bed681d38d85fc966de5f77c
Payton Bates <paytoncbates@gmail.com>
2025-05-11 20:12:10 -0400

    feat: Implement routes for comments

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 import authRoutes from "./auth.js"
 import studySpotsRoutes from "./studySpots.js"
 import userRoutes from "./users.js"
+import commentRoutes from "./comments.js"
 import reviewRoutes from "./reviews.js"
 import forumPostsRoutes from "./forumPosts.js"
 
 const constructorMethod = (app) => {
-    app.use('/', authRoutes);
-    app.use('/', studySpotsRoutes);
-    app.use('/', userRoutes);
+	app.use('/', authRoutes);
+	app.use('/', studySpotsRoutes);
+	app.use('/', userRoutes);
+	app.use('/comments', commentRoutes);
 	app.use('/reviews', reviewRoutes);
-    app.use('/forums', forumPostsRoutes);
-    app.use('*', (req, res) => {
-        return res.status(404).json({error: 'Not found'});
-    });
+	app.use('/forums', forumPostsRoutes);
+	app.use('*', (req, res) => {
+		return res.status(404).json({error: 'Not found'});
+	});
 };
   
 export default constructorMethod;
