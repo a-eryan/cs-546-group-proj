6ef419c492fc3c5af8c769ad534baf840b139b8d
andrewanan <andrew.anantharajah252@gmail.com>
2025-05-13 19:39:36 -0400

    fix: adds reviewComments route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import userRoutes from "./users.js"
 import commentRoutes from "./comments.js"
 import reviewRoutes from "./reviews.js"
 import forumPostsRoutes from "./forumPosts.js"
+import reviewCommentsRoutes from "./reviewComments.js"
 
 const constructorMethod = (app) => {
 	app.use('/', authRoutes);
@@ -12,7 +13,7 @@ const constructorMethod = (app) => {
 	app.use('/comments', commentRoutes);
 	app.use('/reviews', reviewRoutes);
 	app.use('/forums', forumPostsRoutes);
-	app.use(./reviewComments.js);
+	app.use('./review-comments',reviewCommentsRoutes);
 	app.use('*', (req, res) => {
 		return res.status(404).json({error: 'Not found'});
 	});
