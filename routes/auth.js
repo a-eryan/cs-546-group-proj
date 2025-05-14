4aca8d7e06cebf80ddefb38c0579c932b54480a8
Payton Bates <paytoncbates@gmail.com>
2025-05-14 05:53:22 -0400

    refactor: rewrite auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,97 +1,83 @@
-//allow only stevens.edu domains to log in/register
-//docs: https://developers.google.com/identity
-import { Router } from "express";
-import { checkEmail, checkPassword } from "../helpers.js";
-import { login, register } from "../data/users.js";
+import { Router } from 'express';
+import { checkEmail, checkPassword } from '../helpers.js';
+import { login, register } from '../data/users.js';
 import xss from 'xss';
 
 const router = Router()
 
-router
-    .route('/register')
-    .get(async(req, res) => {
-        //code here for GET
-        try {
-            return res.render('register')
-        } catch (e) {
-            return res.status(500).json({error: e})
-        }
-    })
-    .post(async(req, res) => {
-        try {
-            let email = xss(req.body.email)
-            let password = xss(req.body.password)
-            let confirmPassword = xss(req.body.confirmPassword)
+// GET the login page
+router.get('/login', (req, res) => {
+	try {
+		return res.render('login');
+	} catch (e) {
+		return res.status(500).json({ error: e });
+	}
+});
 
-            email = checkEmail(email)
-            password = checkPassword(password)
-            confirmPassword = checkPassword(confirmPassword)
+// POST the login form
+router.post('/login', async (req, res) => {
+	try {
+		// Validate the email and password
+		const email = checkEmail(xss(req.body.email));
+		const password = checkPassword(xss(req.body.password));
 
-            if (password !== confirmPassword){
-                return res.status(400).render('register', {
-                  error: "The passwords do not match"
-                })
-            }
+		// Log the user in and redirect them to the study spots page
+		req.session.user = await login(email, password);
+		return res.redirect('/studyspots');
+	} catch (e) {
+		return res.status(400).render('login', { error: e });
+	}
+});
 
-						try {
-							await register(email, password);
-							return res.redirect('/login');
-						} catch (e) {
-							return res.status(400).render('register', { error: e });
-						}
-        } catch (e) {
-            console.log(e)
-            return res.status(400).render('register', { error: e})
-        }
-    })
+// GET the registration page
+router.get('/register', (req, res) => {
+	try {
+		return res.render('register');
+	} catch (e) {
+		return res.status(500).json({ error: e });
+	}
+});
 
-router
-    .route('/login')
-    .get(async(req, res) => {
-        try {
-            return res.render('login')
-        } catch (e) {
-            return res.status(500).json({error: e})
-        }
-    })
-    .post(async(req, res) => {
-        try {
-            let email = xss(req.body.email)
-            let password = xss(req.body.password)
-            if (!email || !password){
-                return res.status(400).render('login', {
-                    error: 'Email or password not provided'
-                })
-            }
+// POST the registration form
+router.post('/register', async (req, res) => {
+	try {
+		// Validate the passwords
+		const password = checkPassword(xss(req.body.password));
+		const confirmPassword = xss(req.body.confirmPassword); // No need to checkPassword
 
-            email = checkEmail(email)
-            password = checkPassword(password)
+		if (password !== confirmPassword)
+			return res.status(400).render('register', { error: "Passwords do not match" });
 
-            req.session.user = await login(email, password);
+		// Register the user and redirect to the login page
+		const email = checkEmail(xss(req.body.email));
 
-            return res.redirect('/studyspots')
-        } catch (e) {
-            console.log(e)
-            return res.status(400).render('login', { error: e })
-        }
-    })
+		await register(email, password);
+		return res.redirect('/login');
+	} catch (e) {
+		return res.status(400).render('register', { error: e });
+	}
+});
 
-router.route('/logout').get(async (req, res) => {
-  try {
-    req.session.destroy(err => {
-      if (err) {
-        return res.status(500).render('error', { error: 'Failed to logout. Please try again.' });
-      }
+// GET the logout page
+router.get('/logout', (req, res) => {
+	try {
+		// Destroy the session
+		req.session.destroy(err => {
+			if (err) // Render the error page if session destruction fails
+				return res.status(500).render('error', { error: "Failed to logout, please try again" });
+			
+			// Clear the cookie
+			res.set('Cache-Control', 'no-store');
+			res.set('Pragma', 'no-cache');
+			res.set('Expires', '0');
+			res.clearCookie('AuthenticationState');
 
-      res.set('Cache-Control', 'no-store');
-      res.set('Pragma', 'no-cache');
-      res.set('Expires', '0');
-      res.clearCookie('AuthenticationState');
-      return res.redirect('/login');
-    });
-  } catch (e) {
-    return res.status(500).render('error', { error: 'Internal Server Error' });
-  }
+			// Redirect to login
+			return res.redirect('/login');
+		});
+	} catch (e) {
+		return res.status(500).json({ error: e });
+	}
 });
 
 export default router
